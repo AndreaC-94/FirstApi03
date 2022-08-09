a613c999583af0cdb3ca99b86a9577eaecd93f1f
Andrea <andreacolucci1994@gmail.com>
2022-08-09 17:29:53 +0200

    code cleaning

--- src/main/java/co/develhope/FirstAPI03/StringController.java
+++ src/main/java/co/develhope/FirstAPI03/StringController.java
@@ -7,21 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StringController {
 
-//    @GetMapping ("/{first}/{second}")
-//    public String concatenation(@PathVariable(name = "first", required = true) String first, @PathVariable(name = "second", required = false) String second){
-//        if(second == null) return "Concatenation: " + first;
-//        else return "Concatenation: " + first + second;
-//    }
-
-    /*Come dovrei fare a farlo funzionare con una singola chiamata?*/
-
     @GetMapping ("/{first}")
-    public String concatenation(@PathVariable(name = "first", required = true) String first){
+    public String concatenation(@PathVariable(name = "first") String first){
         return "Concatenation: " + first;
     }
 
     @GetMapping ("/{first}/{second}")
-    public String concatenation2(@PathVariable(name = "first", required = true) String first, @PathVariable(name = "second", required = true) String second){
+    public String concatenation2(@PathVariable(name = "first") String first, @PathVariable(name = "second") String second){
         return "Concatenation: " + first + second;
     }
 }
